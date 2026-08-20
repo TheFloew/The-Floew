7cb8f97bd5a23bdd736131c3aca7e3d73e1210c7
The Flöw <thefloewback@gmail.com>
2026-08-21 01:05:42 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 window.__floewAppStarted=true;
-window.__floewAppVersion="31.56.0";
+window.__floewAppVersion="31.57.0";
 const FLOEW_CONFIG=window.FLOEW_CONFIG||{};
 const NEWS_WORKER_BASE=String(
   FLOEW_CONFIG.newsWorkerBase||"https://thefloew.thefloewback.workers.dev"
@@ -2462,7 +2462,7 @@ function timeText(v){
 
 
 const storyMediaCache=new Map();
-const VIDEO_RESOLVER_VERSION="20260821-2";
+const VIDEO_RESOLVER_VERSION="20260821-3";
 const SUPPORTED_EMBED_VIDEO_PROVIDERS=new Set([
   "youtube",
   "vimeo",
