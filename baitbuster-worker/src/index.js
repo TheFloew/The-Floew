5fc2ebdd871968ff896b3b405455e4691d090ae4
The Flöw <thefloewback@gmail.com>
2026-09-23 05:56:57 +0300

    chore: bump BaitBuster Worker to 1.6.2

--- baitbuster-worker/src/index.js
+++ baitbuster-worker/src/index.js
@@ -12,7 +12,7 @@ import {
 } from "./ai.js";
 
 const SERVICE="thefloew-baitbuster";
-const VERSION="1.6.1";
+const VERSION="1.6.2";
 const ALLOWED_ORIGIN="https://xn--flw-tna.tr";
 const MAX_STORIES=12;
 const CACHE_TTL_SECONDS=30*24*60*60;
