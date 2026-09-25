412392b0f03af4a1115ce6229a5385e53aa65630
The Flöw <thefloewback@gmail.com>
2026-09-25 15:46:33 +0300

    test: expect BaitBuster client v13

--- baitbuster-worker/test/client-ui.test.mjs
+++ baitbuster-worker/test/client-ui.test.mjs
@@ -105,7 +105,7 @@ test("production homepage includes the approved BaitBuster integration",async()=
   assert.match(html,/id="baitbuster-setting"/);
   assert.match(html,/\.baitbuster-rewrite-mark\{/);
   assert.match(html,/js\/baitbuster-ui\.js\?v=5/);
-  assert.match(html,/js\/baitbuster-beta\.js\?v=12/);
+  assert.match(html,/js\/baitbuster-beta\.js\?v=13/);
   assert.doesNotMatch(html,/name="robots"[^>]*noindex/i);
 });
 
